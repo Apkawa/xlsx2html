1cf3df162bf3c08cdc7ddf56c6b4e6ceeb6364b0
apkawa <apkawa@gmail.com>
2016-09-06 13:27:21 +0300

    drop py3.2

--- setup.py
+++ setup.py
@@ -39,5 +39,8 @@ setup(
         'Topic :: Internet :: WWW/HTTP',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.4',
     ],
 )
