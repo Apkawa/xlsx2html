e4d340edb1a9128201b32ad4847c7d0d12ce44bf
apkawa <apkawa@gmail.com>
2017-10-17 19:14:32 +0300

    add coveralls and flake8

--- tests/test_format.py
+++ tests/test_format.py
@@ -1,23 +1,24 @@
 # coding: utf-8
 from __future__ import unicode_literals
 
-import tempfile
-
 import os
 from unittest import TestCase
+from xlsx2html.format import format_decimal
 
 FIXTURES_ROOT = os.path.join(os.path.dirname(__file__), 'fixtures')
-
-from xlsx2html.format import format_decimal
 
 
 class FormatTestCase(TestCase):
     maxDiff = None
+
     def setUp(self):
         pass
 
     def test_currency_format(self):
         format = u'#,##0.00\\ [$\u0440.-419];\\-#,##0.00\\ [$\u0440.-419]'
-        self.assertEqual(format_decimal(1000, format=format, locale='ru'), '1\xa0000,00 р.')
-        self.assertEqual(format_decimal(-1000, format=format, locale='ru'), '-1\xa0000,00 р.')
-
+        self.assertEqual(
+            format_decimal(1000, format=format, locale='ru'),
+            '1\xa0000,00 р.')
+        self.assertEqual(
+            format_decimal(-1000, format=format, locale='ru'),
+            '-1\xa0000,00 р.')
